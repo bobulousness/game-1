28d4c22b5bb5a3a47c288383d52c95c1fd0e8c44
SkilStak Coding Arts <learn@skilstak.com>
2015-07-18 11:56:34 -0400

    save

--- shooter/game.js
+++ shooter/game.js
@@ -1,1 +1,21 @@
-console.log("I am really working"); 
+'use scrict';
+
+var game = new Phaser.Game(800,600,Phaser.AUTO,'game',
+  {preload:preload,create:create,update:update,render:render});
+
+function preload() {
+  game.stage.backgroundcolor = '#101010';
+  game.load.image('diamonds_ship','diamond_ship.gif');
+}
+
+var diamonds_ship;
+function create () {
+ diamonds_ship = game.add.sprite(400,550,'diamonds_ship');
+ }
+
+function update() {
+}
+
+function render() {
+}
+  
