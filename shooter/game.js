d808fae5e007c52801eb9bd25936fccc9650cc53
SkilStak Coding Arts <learn@skilstak.com>
2015-08-01 11:57:13 -0400

    save

--- shooter/game.js
+++ shooter/game.js
@@ -6,14 +6,18 @@ var game = new Phaser.Game(800,600,Phaser.AUTO,'game',
 var diaship;
 var cursors;
 var strsky;
+var diabul;
 
 function preload(){ 
   game.load.image('strsky','strsky.gif');
   game.load.image('diaship','diamond_ship.gif');
+  game.load.image('diabul','diabul.gif');
 }
 
 function create () {
  strsky = game.add.tileSprite(0,0,800,600,'strsky');
+ strsky.autoScroll(-500,0);
+ diabul = game.add.sprite(20,300,'diabul');
  diaship = game.add.sprite(20,300,'diaship');
  game.physics.enable(diaship, Phaser.Physics.ARCADE);
  cursors = game.input.keyboard.createCursorKeys();
@@ -22,6 +26,10 @@ function create () {
 function update() {
   diaship.body.velocity.x = 0;
   diaship.body.velocity.y = 0;
+
+  diabul.x = diaship.x + 90;
+  diabul.y = diaship.y + 70;
+
   if (cursors.up.isDown) {
       diaship.body.velocity.y = -300;
     }
@@ -29,7 +37,6 @@ function update() {
     diaship.body.velocity.y = 300;
     }
 }
-
 function render() {
 }
   
