0a9916b0c964ad69ef1ea669119edf830abe1cd2
SkilStak Coding Arts <learn@skilstak.com>
2015-08-01 10:36:36 -0400

    save

--- shooter/game.js
+++ shooter/game.js
@@ -3,13 +3,14 @@
 var game = new Phaser.Game(800,600,Phaser.AUTO,'game',
   {preload:preload,create:create,update:update,render:render});
 
-function preload() {
-  game.stage.backgroundcolor = '#101010';
-  game.load.image('diamonds_ship','diamond_ship.gif');
-}
-
 var diamonds_ship;
 var cursors;
+var strsky;
+
+function preload() 
+  game.load.image('strsky', 'strsky.gif');
+  game.load.image('diamonds_ship','diamond_ship.gif');
+}
 
 function create () {
  diamonds_ship = game.add.sprite(20,300,'diamonds_ship');
